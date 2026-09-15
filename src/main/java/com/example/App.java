1b0195d60a25c6601abb27d5ad9c33875fbfd179
Adarsh Chandran kv <144888670+Adarsh347@users.noreply.github.com>
2026-09-15 11:53:19 +0530

    Create App class with main method and message output

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        AppText text = new AppText();
+
+        System.out.println(text.getMessage());
+    }
+}
